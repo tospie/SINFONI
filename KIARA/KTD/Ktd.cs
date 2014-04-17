16fe08dd05fb1c1730fa84c5243b7a8883f5e22a
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-17 14:34:31 +0200

    Implemented KTD Instance (with slight renamings)

--- KIARA/KTD/Ktd.cs
+++ KIARA/KTD/Ktd.cs
@@ -5,18 +5,30 @@ using System.Text;
 
 namespace KIARA
 {
-    public class Ktd
+    public class KTD
     {
-        public static Ktd KTD;
+        public static KTD Instance = new KTD();
 
         public bool ContainsType(string name)
         {
-            throw new NotImplementedException("Function ContainsType is not implemented yet");
+            return registeredTypes.ContainsKey(name);
         }
 
         public KtdType GetType(string name)
         {
-            throw new NotImplementedException("Function GetType is not implemented yet");
+            if (!ContainsType(name))
+            {
+                throw new Exception("KTD does not contain Type Description with name " + name);
+            }
+
+            return registeredTypes[name];
         }
+
+        public void RegisterType(string name, KtdType type)
+        {
+            registeredTypes.Add(name, type);
+        }
+
+        private Dictionary<string, KtdType> registeredTypes = new Dictionary<string,KtdType>();
     }
 }
