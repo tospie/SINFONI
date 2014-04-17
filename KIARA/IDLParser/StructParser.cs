c4d05e366bd1e002130e923776048746cbce0e82
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-17 14:35:03 +0200

    Struct Parser registeres newly created types to KTD

--- KIARA/IDLParser/StructParser.cs
+++ KIARA/IDLParser/StructParser.cs
@@ -18,6 +18,8 @@ namespace KIARA
             {
                 createKtdTypeForMember(memberDef, newStruct);
             }
+
+            KTD.Instance.RegisterType(structName, newStruct);
         }
 
         private string parseName(string structDefinition)
