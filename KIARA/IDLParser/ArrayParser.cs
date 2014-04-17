bd05d04750b0e76e6549b636c67860a3cdc368a4
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-17 14:34:08 +0200

    Added Array Parser class

---
+++ KIARA/IDLParser/ArrayParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace KIARA
+{
+    public class ArrayParser
+    {
+        public KtdArray ParseArray(string arrayDefinition)
+        {
+            KtdArray result = new KtdArray();
+
+            int indexStart = arrayDefinition.IndexOf('<') + 1;
+            int indexEnd = arrayDefinition.IndexOf('>');
+            string elementType = arrayDefinition.Substring(indexStart, indexEnd - indexStart);
+            
+            result.elementType = KTD.Instance.GetType(elementType);
+            return result;
+        }
+    }
+}
