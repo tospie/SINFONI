2c62a48d9d49d0d7c72a267d18741451f3423425
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-17 12:13:42 +0200

    Added IDL Unit Test Project

---
+++ KIARAUnitTests/TestIDLParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KIARA;
+using NUnit.Framework;
+
+namespace KIARAUnitTests
+{
+    [TestFixture()]
+    class TestIDLParser
+    {
+        [Test()]
+        public void testRegexParsing()
+        {
+            var parser = new IDLParser();
+            parser.parseIDL();
+        }
+    }
+}
