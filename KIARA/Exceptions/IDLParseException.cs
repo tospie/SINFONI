3a306bdab446ab02c1dec644d8e5a3e95ebbf621
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-22 10:08:42 +0200

    Added IDLParseException class

---
+++ KIARA/Exceptions/IDLParseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA.Exceptions
+{
+    public class IDLParseException : Exception
+    {
+        public IDLParseException(string idlFragment, string fragmentType)
+            : base("Failed to parse IDL Fragment as " + fragmentType + ": " + idlFragment)
+        {}
+    }
+}
