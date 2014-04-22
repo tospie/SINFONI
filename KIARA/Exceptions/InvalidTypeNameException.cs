4f6ecf2d3e36c5141f076b6d04c7afde37d97f13
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-22 10:30:41 +0200

    Added InvalidTypenameException

---
+++ KIARA/Exceptions/InvalidTypeNameException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA.Exceptions
+{
+    public class InvalidTypeNameException : Exception
+    {
+        public InvalidTypeNameException()
+            : base("Failed to register type as name is either null or contains invalid characters")
+        { }
+    }
+}
