a76bfb24442acf0bd53d09f4e7780c5df973b5c3
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-22 10:31:13 +0200

    Added Comments to KTD

--- KIARA/KTD/Ktd.cs
+++ KIARA/KTD/Ktd.cs
@@ -6,8 +6,15 @@ using KIARA.Exceptions;
 
 namespace KIARA
 {
+    /// <summary>
+    /// KTD (KIARA Type Description) maintains all types registered fron a KIARA IDL. KTD contains all base types
+    /// supported by KIARA. During runtime, new complex types derived from these base types can be registered.
+    /// </summary>
     public class KTD
     {
+        /// <summary>
+        /// Singleton handler to the Kiara Type Description
+        /// </summary>
         public static KTD Instance = new KTD();
 
         private KTD()
@@ -15,11 +22,22 @@ namespace KIARA
             registerBaseTypes();
         }
 
+        /// <summary>
+        /// Checks if a type with a specific name is registered in the KTD
+        /// </summary>
+        /// <param name="name">Name of the type that should be checked</param>
+        /// <returns>true, if type was registered before</returns>
         public bool ContainsType(string name)
         {
             return registeredTypes.ContainsKey(name);
         }
 
+        /// <summary>
+        /// Returns a <see cref="KtdType"/> object that is registered in the KTD under the specified name.
+        /// Throws a <see cref="TypeNotRegisteredException"/>, if a type with given name is not registered.
+        /// </summary>
+        /// <param name="name">Name of the type that should be returned</param>
+        /// <returns></returns>
         public KtdType GetKtdType(string name)
         {
             if (!ContainsType(name))
@@ -30,6 +48,11 @@ namespace KIARA
             return registeredTypes[name];
         }
 
+        /// <summary>
+        /// Registers a new Type to the Ktd. The type object that is used for registration must contain a
+        /// valid name (not null, not registered before). Otherwise, KTD will throw an exception.
+        /// </summary>
+        /// <param name="type">New type that should be registered to the KTD</param>
         public void RegisterType(KtdType type)
         {
             if(type.Name == null
