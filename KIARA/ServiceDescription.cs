74c2db3bccc971f800a6d87a53b878a5112f9476
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-23 15:35:38 +0200

    Added a Service Description class

---
+++ KIARA/ServiceDescription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA
+{
+    public class ServiceDescription
+    {
+        string Name { get; internal set; }
+        KtdType ReturnType { get; internal set; }
+
+        internal Dictionary<string, KtdType> parameters;
+    }
+}
