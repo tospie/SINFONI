cfb23ec426b4b07ef1e0109bffef1a45e2679ef2
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-24 09:10:57 +0200

    Added a service parser class

---
+++ KIARA/IDLParser/ServiceParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace KIARA.IDLParser
+{
+    internal class ServiceParser
+    {
+        internal string parseName(string serviceDefinition)
+        {
+            Regex nameRegEx = new Regex("struct ([A-Za-z0-9_]*) [{}._<,>; ]*");
+            Match nameMatch = nameRegEx.Match(serviceDefinition);
+            string name = nameMatch.Groups[1].Value;
+            return name;
+        }
+
+    }
+}
