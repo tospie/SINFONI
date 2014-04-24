e305acb481653400880fca29430bfb26f713f753
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-24 09:08:50 +0200

    Added KIARA Service Class

---
+++ KIARA/KTD/KiaraService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA.KTD
+{
+    public class KiaraService
+    {
+        Dictionary<string, ServiceFunctionDescription> serviceFunctions =
+            new Dictionary<string, ServiceFunctionDescription>();
+    }
+}
