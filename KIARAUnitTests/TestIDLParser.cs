84f6ef7fa3f2fe94abcf39816a6f8c88e268b71c
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-24 12:34:51 +0200

    Moved example IDL from parser to unit tests

--- KIARAUnitTests/TestIDLParser.cs
+++ KIARAUnitTests/TestIDLParser.cs
@@ -11,6 +11,82 @@ namespace KIARAUnitTests
     [TestFixture()]
     class TestIDLParser
     {
+        #region ExampleIDL
+        const string exampleIDL = @"struct AttributeDef {
+                            string Guid;
+                            // Strange Name
+                            string Name; /* This is some strange name assigned to the line */
+                            any DefaultValue; // Some stuff
+
+                            string Type;  // contains AssemblyQualifiedName of the type
+                        }
+
+                        struct ComponentDef {
+                            string Guid;
+                            string Name;
+                            array<AttributeDef> AttributeDefs;
+                        }
+
+                        struct AttributeSyncInfo {
+                            i64 LastTimestamp;
+                            string LastSyncID;
+                            any LastValue;
+                        }
+
+                        struct ComponentSyncInfo {
+                            map<string, AttributeSyncInfo> Attributes;
+
+
+                        }
+
+                        struct EntitySyncInfo {
+
+                            map<string, ComponentSyncInfo> Components;
+
+                        }
+
+                        service serverSync
+                        {
+                            // Returns this server's sync ID.
+                            string getSyncID();
+
+                            // Called to notify that an entity has been added to the caller's world. Receiving side should create a new entity
+                            // if it isn't present in the world and use provided initialSyncInfo as a starting sync info. If the entity exists
+                            // already, this message should be processed as changeAttributes, where each individual attribute of an entity is
+                            // synchronized.
+                            void addEntity(string guid, EntitySyncInfo initialSyncInfo);
+
+                            // Called to notify that an entity has been removed from the caller's world. If the entity is not present on the
+                            // receiving side, this messages should be ignored (except relay nodes, which still should relay it further).
+                            // @param guid Guid of the removed entity.
+                            void removeEntity(string guid);
+
+                            // Called to notify that a set of entity's attributes has been modified in the caller's world. The receiving side
+                            // should process each attribute and update local attributes where necessary.
+                            void changeAttributes(string guid, EntitySyncInfo changedAttributes);
+
+                            // Called to notify the other parties that a plugin handling given component definition has been loaded remotely.
+                            // The receving side should register the component locally and remove this component from the deferred plugins'
+                            // dependencies.
+                            void registerComponentDefinition(ComponentDef definition);
+
+                            // Returns serialized version of the remote server's domain-of-responsibility.
+                            string getDoR();
+
+                            // Returns serialized version of the remote server's domain-of-interest.
+                            string getDoI();
+
+                            // Called by the remote server to notify on changes to its domain-of-reponsibility, serialized version of which is
+                            // passed as a parameter.
+                            void updateDoI(string newDoI);
+
+                            // Called by the remote server to notify on changes to its domain-of-interest, serialized version of which is
+                            // passed as a parameter.
+                            void updateDoR(string newDoR);
+                        }
+";
+        #endregion
+
         [SetUp()]
         public void SetUp()
         {
@@ -226,7 +302,7 @@ namespace KIARAUnitTests
         [Test()]
         public void ShouldParseExampleIDLWithoutError()
         {
-            Assert.DoesNotThrow(() => IDLParser.Instance.parseIDL());
+            Assert.DoesNotThrow(() => IDLParser.Instance.parseIDL(exampleIDL));
             var services = ServiceRegistry.Instance.services;
             var types = KTD.Instance.registeredTypes;
         }
