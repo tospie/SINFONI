534257d92ad8a3dde630a8bee740f6d693f77549
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-24 11:08:57 +0200

    Added exception when trying to access services that are not registered

---
+++ KIARA/Exceptions/ServiceNotRegisteredException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA.Exceptions
+{
+    class ServiceNotRegisteredException : Exception
+    {
+        public ServiceNotRegisteredException(string serviceName)
+            : base("Service or ServiceFunction with name " + serviceName + " is not registered")
+        { }
+    }
+}
