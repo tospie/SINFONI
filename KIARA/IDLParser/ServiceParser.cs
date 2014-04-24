1f2a0b3806fbe1650fc5c94be2f07ab337b22543
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-24 10:11:59 +0200

    Fixed Regex to determine service name

--- KIARA/IDLParser/ServiceParser.cs
+++ KIARA/IDLParser/ServiceParser.cs
@@ -10,7 +10,7 @@ namespace KIARA
     {
         internal string parseName(string serviceDefinition)
         {
-            Regex nameRegEx = new Regex("struct ([A-Za-z0-9_]*) [{}._<,>; ]*");
+            Regex nameRegEx = new Regex("service ([A-Za-z0-9_]*)[{}._<,>; ]*");
             Match nameMatch = nameRegEx.Match(serviceDefinition);
             string name = nameMatch.Groups[1].Value;
             return name;
