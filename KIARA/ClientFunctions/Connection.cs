e35b7f6ecff31752cd5f290d2b4659834b562ec0
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-25 14:39:03 +0200

    Added stubs for IClientFunctionCall and Connection which will later pretty much behave like Sergiy's code

---
+++ KIARA/ClientFunctions/Connection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KIARA.Exceptions;
+
+namespace KIARA
+{
+    public class Connection
+    {
+        // TODO: Change Return type to ICLientFunctionCall as soon as the calling mechanism is implemented
+        public delegate void ClientFunction(params object[] parameters);
+
+        public ClientFunction GenerateClientFunction(string serviceName, string functionName)
+        {
+            return (ClientFunction)delegate(object[] parameters)
+            {
+                KiaraService registeredService = ServiceRegistry.Instance.GetService(serviceName);
+                ServiceFunctionDescription registeredServiceFunction = registeredService.GetServiceFunction(functionName);
+
+                if (!registeredServiceFunction.CanBeCalledWithParameters(parameters))
+                {
+                    throw new ParameterMismatchException(
+                        "Could not call Service Function " + serviceName + "." + functionName
+                            + ". The provided parameters can not be mapped to the parameters specified in the IDL.");
+                }
+                // TODO: Implement Calling remote functions
+                // return CallClientFunction(serviceName, parameters);
+            };
+        }
+    }
+}
