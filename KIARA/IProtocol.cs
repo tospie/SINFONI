b7fa7326bf0fdbb379b9b7979e5e6e65405b474a
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-28 12:07:58 +0200

    Added protocol interface

---
+++ KIARA/IProtocol.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA
+{
+    public interface IProtocol
+    {
+        void WriteStructBegin(string structName);
+        void WriteStructEnd();
+
+        void WriteFieldBegin(string fieldName);
+        void WriteFieldEnd();
+
+        void WriteArrayBegin();
+        void WriteArrayEnd();
+
+        void WriteMapBegin();
+        void WriteMapEnd();
+
+        void WriteMessageValue<T>(object value);
+    }
+}
