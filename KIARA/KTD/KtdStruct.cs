c1493a7a2d7bfa1fa707db9bfb5cddd287981721
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-28 09:53:18 +0200

    Added KtdStruct class that now implements the struct specific logic that was previously contained in KtdType base class

---
+++ KIARA/KTD/KtdStruct.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace KIARA
+{
+    public class KtdStruct : KtdType
+    {
+        public KtdStruct(string name) : base(name) { }
+
+        public object AssignValuesFromObject(object other)
+        {
+            var result = 0;
+
+            mappings.Add(other.GetType(), (MappingFunction)delegate(object other2)
+            {
+                return this.MapByName(other2);
+            });
+
+            if (canBeAssignedFromType(other.GetType()))
+            {
+                // Map object by name-type-matching
+            }
+            else /* if valid mapping is declared */
+            {
+                // perform mapping as declared in the mapping function
+            }
+
+            MappingFunction map = mappings[other.GetType()] as MappingFunction;
+            map(other);
+
+            return result;
+        }
+
+        bool MapByName(object other)
+        {
+            other.GetType().GetMembers();
+
+            return true;
+        }
+
+        internal override bool canBeAssignedFromComplexType(Type type)
+        {
+            if (validMappings.ContainsKey(type))
+                return validMappings[type];
+
+            else
+                return validMappingForTypeExists(type);
+        }
+
+        private bool validMappingForTypeExists(Type type)
+        {
+            var fields = type.GetFields();
+            var properties = type.GetProperties();
+
+            foreach (KeyValuePair<string, KtdType> member in members)
+            {
+                bool memberCanBeAssigned =
+                    memberCanBeAssignedFromProperties(member, properties)
+                    || memberCanBeAssignedFromFields(member, fields);
+
+                if (!memberCanBeAssigned)
+                {
+                    validMappings[type] = false;
+                    return false;
+                }
+            }
+
+            validMappings[type] = true;
+            return true;
+        }
+
+        private bool memberCanBeAssignedFromFields(KeyValuePair<string, KtdType> member, FieldInfo[] fieldInfo)
+        {
+            int indexOfMemberInArray = Array.FindIndex(fieldInfo,
+                delegate(FieldInfo element)
+                {
+                    bool containsElement = element.Name.Equals(member.Key);
+                    return containsElement;
+                });
+
+            if (indexOfMemberInArray == -1)
+                return false;
+
+            var field = fieldInfo[indexOfMemberInArray];
+
+            if (!member.Value.canBeAssignedFromType(field.FieldType))
+                return false;
+
+            return true;
+        }
+
+        private bool memberCanBeAssignedFromProperties(KeyValuePair<string, KtdType> member, PropertyInfo[] propertyInfo)
+        {
+            int indexOfMemberInArray = Array.FindIndex(propertyInfo,
+                delegate(PropertyInfo element)
+                {
+                    bool containsElement = element.Name.Equals(member.Key);
+                    return containsElement;
+                });
+
+            if (indexOfMemberInArray == -1)
+                return false;
+
+            var property = propertyInfo[indexOfMemberInArray];
+
+            if (!member.Value.canBeAssignedFromType(property.PropertyType))
+                return false;
+
+            return true;
+        }
+
+        internal Dictionary<string, KtdType> members = new Dictionary<string, KtdType>();
+        internal Dictionary<Type, bool> validMappings = new Dictionary<Type, bool>();
+        internal Dictionary<Type, Delegate> mappings = new Dictionary<Type, Delegate>();
+    }
+}
