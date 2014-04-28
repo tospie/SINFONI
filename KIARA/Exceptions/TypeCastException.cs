93c866e119bfc7f308cda52dbefbb7efbc8b4e86
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-28 09:55:04 +0200

    Added Typecast exception that will be thrown when type that is created as basetype has no basetype name

---
+++ KIARA/Exceptions/TypeCastException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA.Exceptions
+{
+    public class TypeCastException : Exception
+    {
+        public TypeCastException(string message) : base(message) { }
+    }
+}
