5837e898973bec46e8d0c42dae1ba5e0b94216b7
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-29 10:26:50 +0200

    Added function that assigns a C# Native Array to a KtdArray

--- KIARA/KTD/KtdArray.cs
+++ KIARA/KTD/KtdArray.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using KIARA.Exceptions;
 
 [assembly: InternalsVisibleTo("KIARAUnitTests")]
 
@@ -29,6 +30,28 @@ namespace KIARA
             return elementType.canBeAssignedFromType(itemType);
         }
 
+        public override object AssignValuesFromObject(object other)
+        {
+            if(!canBeAssignedFromType(other.GetType()))
+                throw new TypeCastException("Cannot assign value to Instance of type KtdArray<" + elementType.Name + "> : "
+                    + other + " is of type " + other.GetType());
+
+            return CreateArrayInstanceFrom(other);
+        }
+
+        private KtdArrayInstance CreateArrayInstanceFrom(object other)
+        {
+            var array = other as IEnumerable;
+            List<KtdTypeInstance> arrayValues = new List<KtdTypeInstance>();
+
+            foreach (object value in array)
+            {
+                arrayValues.Add((KtdTypeInstance)elementType.AssignValuesFromObject(value));
+            }
+
+            KtdArrayInstance instance = new KtdArrayInstance(this, arrayValues.ToArray());
+            return instance;
+        }
         // From StackOverflow:
         // stackoverflow.com/questions/3922029/how-to-retrieve-the-generic-type-used-in-a-generic-ienumerable-in-net
         private static Type GetEnumerableType(Type type)
