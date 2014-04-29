9c73e973d3e0ce80b2b1d0c5b480cc76cdc223a9
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-29 10:23:35 +0200

    Added KtdTypeInstance class

---
+++ KIARA/KTD/KtdTypeInstance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA
+{
+    public class KtdTypeInstance
+    {
+        public KtdTypeInstance(KtdType type, object value)
+        {
+            TypeDefinition = type;
+            Value = value;
+        }
+
+        public KtdType TypeDefinition { get; internal set; }
+        public object Value { get; internal set; }
+    }
+}
