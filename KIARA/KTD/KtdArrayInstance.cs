baff160250da5a6f3ff936145b5cffbeeb9be01c
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-29 10:25:13 +0200

    Implemented KtdArrayInstance

---
+++ KIARA/KTD/KtdArrayInstance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA
+{
+    public class KtdArrayInstance
+    {
+        public KtdArrayInstance(KtdType definition, KtdTypeInstance[] values)
+        {
+            TypeDefinition = definition;
+            Values = values;
+        }
+        public KtdType TypeDefinition;
+        public KtdTypeInstance[] Values { get; internal set; }
+    }
+}
