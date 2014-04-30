731006da6a38dd52cffe68430f426d138a6eefb8
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-30 11:08:38 +0200

    Added map instance class

---
+++ KIARA/KTD/KtdMapInstance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA
+{
+    public class KtdMapInstance
+    {
+        public KtdMapInstance(KtdMap type, Dictionary<object, object> values)
+        {
+            TypeDefinition = type;
+            Values = values;
+        }
+
+        public KtdType TypeDefinition { get; private set; }
+        public Dictionary<object, object> Values { get; private set; }
+    }
+}
