b83d27dede761105877bba66ed1bc4a1bbe7554e
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-04-30 13:20:13 +0200

    Implemented class for KtdStructInstance

--- KIARA/KTD/KtdStructInstance.cs
+++ KIARA/KTD/KtdStructInstance.cs
@@ -7,5 +7,19 @@ namespace KIARA
 {
     public class KtdStructInstance : KtdTypeInstance
     {
+        public KtdStructInstance(KtdType type, Dictionary<string, KtdTypeInstance> fields)
+        {
+            TypeDefinition = type;
+            fieldValues = fields;
+        }
+
+        public KtdType TypeDefinition { get; private set; }
+        public Dictionary<string, KtdTypeInstance> Fields
+        {
+            get { return fieldValues; }
+            set { fieldValues = value; }
+        }
+        private Dictionary<string, KtdTypeInstance> fieldValues
+            = new Dictionary<string, KtdTypeInstance>();
     }
 }
