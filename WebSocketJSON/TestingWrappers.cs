42512b2504fc11eb2541e44fa279944c3c8721f1
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-05-09 09:52:24 +0200

    Added original WebSocket JSON protocol project and adapted it to the data structures used in new KIARA implementation

---
+++ WebSocketJSON/TestingWrappers.cs
@@ -0,0 +1,47 @@
+using System;
+using WebSocket4Net;
+using SuperSocket.ClientEngine;
+using KIARA;
+
+namespace WebSocketJSON
+{
+    public interface IWSJServerFactory
+    {
+        IWSJServer Construct(Action<Connection> onNewClient);
+    }
+
+    public class WSJServerFactory : IWSJServerFactory
+    {
+        public IWSJServer Construct(Action<Connection> onNewClient)
+        {
+            return new WSJServer(onNewClient);
+        }
+    }
+
+    public interface IWSJFuncCallFactory
+    {
+        IWSJFuncCall Construct();
+    }
+
+    public class WSJFuncCallFactory : IWSJFuncCallFactory
+    {
+        public IWSJFuncCall Construct()
+        {
+            return new WSJFuncCall();
+        }
+    }
+
+    public interface IWebSocketFactory
+    {
+        ISocket Construct(string uri);
+    }
+
+    public class WebSocketFactory : IWebSocketFactory
+    {
+        public ISocket Construct(string uri)
+        {
+            return new WebSocketSocketAdapter(uri);
+        }
+    }
+}
+
---
+++ WebSocketJSON/WebSocketSocketAdapter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSocket4Net;
+using SuperSocket.ClientEngine;
+
+namespace WebSocketJSON
+{
+    public class WebSocketSocketAdapter : WebSocket, ISocket
+    {
+        public WebSocketSocketAdapter(string uri)
+            : base(uri)
+        {
+            base.MessageReceived += HandleMessageReceived;
+            base.Error += HandleError;
+        }
+
+        private void HandleError(object sender, ErrorEventArgs e)
+        {
+            if (Error != null)
+                Error(sender, new SocketErrorEventArgs(e.Exception));
+        }
+
+        void HandleMessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            if (Message != null)
+                Message(sender, new MessageEventArgs(e.Message));
+        }
+
+        public event EventHandler<MessageEventArgs> Message;
+        public new event EventHandler<SocketErrorEventArgs> Error;
+
+        public bool IsConnected
+        {
+            get { return base.State == WebSocketState.Open; }
+        }
+    }
+}
