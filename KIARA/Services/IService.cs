7e2e6ef2a5ceaef73a3374e94eb6f8370197b07d
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-05-09 09:50:09 +0200

    Added Files from original KIARA implementation that are used by original WSJ

---
+++ KIARA/Services/IService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA
+{
+    public interface IService
+    {
+        Context Context { get; }
+
+        Delegate this[string name] { set; }
+    }
+}
