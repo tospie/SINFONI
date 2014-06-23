dbed2564865c1ede71a13eb66d5cc7705b79a32f
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-06-23 14:31:11 +0200

    Added simple server project as example

---
+++ Examples/SimpleServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KIARA;
+
+namespace SimpleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleServer server = new SimpleServer();
+        }
+    }
+}
---
+++ Examples/SimpleServer/SimpleServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using KIARA;
+using WebSocketJSON;
+
+namespace SimpleServer
+{
+    public class SimpleServer
+    {
+        public SimpleServer()
+        {
+            string configURI = ServerSyncTools.ConvertFileNameToURI("server.json");
+
+            WSJConnectionFactory connectionFactory = new WSJConnectionFactory();
+            ProtocolRegistry.Instance.RegisterConnectionFactory("websocket-json", connectionFactory);
+            
+            Context.DefaultContext.StartServer(configURI, _ => { });
+        }
+    }
+}
