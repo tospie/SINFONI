931219b48392013f0e5d96641209d8f4ecab68f5
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-07-31 11:13:28 +0200

    Added Project for HttpJSON protocol

---
+++ HttpJSON/HttpJSONConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KIARA;
+
+namespace HttpJSONProtocol
+{
+    class HttpJSONConnection : Connection
+    {
+    }
+}
---
+++ HttpJSON/HttpJSONConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KIARA;
+
+namespace HttpJSONProtocol
+{
+    class HttpJSONConnectionFactory : IConnectionFactory
+    {
+        public void OpenConnection(Server serverConfig, Context context, Action<Connection> onConnected)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartServer(Server serverConfig, Context context, Action<Connection> onNewClient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
