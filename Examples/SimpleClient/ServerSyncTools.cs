79379fa152763ab63d2593ffba87aa95bdc6c817
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-07-31 10:50:37 +0200

    Removed server sync tools method that is not needed any more and caused conflicts

--- Examples/SimpleClient/ServerSyncTools.cs
+++ Examples/SimpleClient/ServerSyncTools.cs
@@ -13,13 +13,11 @@
 // You should have received a copy of the GNU General Public License
 // along with FiVES.  If not, see <http://www.gnu.org/licenses/>.
 using KIARA;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using KIARA;
 
 namespace SimpleClient
 {
@@ -39,25 +37,6 @@ namespace SimpleClient
             string assemblyPath = typeof(SimpleClient).Assembly.Location;
             var configFullPath = Path.Combine(Path.GetDirectoryName(assemblyPath), configFilename);
             return "file://" + configFullPath;
-        }
-
-        /// <summary>
-        /// Configures a JsonSerializer used in a given connection to include types names when the declared type of an
-        /// object is different from the actual type.
-        /// </summary>
-        /// <param name="connection">A given connection.</param>
-        public static void ConfigureJsonSerializer(Connection connection)
-        {
-            object settingsObj;
-            if (connection.GetProperty("JsonSerializerSettings", out settingsObj))
-            {
-                JsonSerializerSettings settings = settingsObj as JsonSerializerSettings;
-                if (settings != null)
-                {
-                    settings.TypeNameHandling = TypeNameHandling.Auto;
-                    connection.SetProperty("JsonSerializerSettings", settings);
-                }
-            }
         }
     }
 }
