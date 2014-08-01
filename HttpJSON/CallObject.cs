3b57521edca74a82d50d0038cfeee8835ee53156
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-08-01 09:01:46 +0200

    Added call object class

---
+++ HttpJSON/CallObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HttpJSONProtocol
+{
+    public class CallObject
+    {
+        public string method;
+        public string jsonrpc = "2.0";
+        public object[] parameters;
+        public int id;
+    }
+}
