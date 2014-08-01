25bff7d3bd75e678b00d5bb49e0b608739da8dca
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-08-01 09:03:37 +0200

    Added HTTPJson Function Call object

---
+++ HttpJSON/HttpJSONFunctionCall.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KIARA;
+
+namespace HttpJSONProtocol
+{
+    public class HttpJSONFunctionCall : FuncCallBase, IClientFunctionCall
+    {
+        public HttpJSONFunctionCall(string serviceName, string methodName)
+            : base(serviceName, methodName)
+        { }
+
+        protected override object ConvertResult(object result, Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
