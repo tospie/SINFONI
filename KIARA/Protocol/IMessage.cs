78403638ec0c335ca0ab64810077f0caaa347d70
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-10-27 15:03:58 +0100

    Moved everything that was formerly handled by object array to Message Interface

--- KIARA/Protocol/IMessage.cs
+++ KIARA/Protocol/IMessage.cs
@@ -5,8 +5,21 @@ using System.Text;
 
 namespace KIARA
 {
+    public enum MessageType
+    {
+        REQUEST,
+        RESPONSE,
+        EXCEPTION
+    }
+
     public interface IMessage
     {
-        object Payload { get; }
+        MessageType Type { get; }
+        int ID { get; }
+        string MethodName { get; }
+        List<object> Parameters { get; }
+        List<int> Callbacks { get; }
+        object Result { get; }
+        bool IsException { get; }
     }
 }
