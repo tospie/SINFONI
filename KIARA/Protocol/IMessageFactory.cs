6ce675aa7dd4b2ffdba22476197747eee70217e4
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-10-27 15:06:22 +0100

    Started design of IMessageFactory

---
+++ KIARA/Protocol/IMessageFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA.Protocol
+{
+    public interface IMessageFactory
+    {
+        IMessage createCallMessage(int callID, string name, List<int> callbacks, List<object> convertedArgs);
+    }
+}
