9b9c978d6202cda21cc0096744d79ef93294160d
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-10-27 15:04:15 +0100

    Base Implementation of Message class

---
+++ KIARA/Protocol/MessageBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA
+{
+    public class MessageBase : IMessage
+    {
+        public MessageType Type { get; private set; }
+
+        public int ID { get; private set; }
+
+        public string MethodName { get; private set; }
+
+        public List<object> Parameters { get; private set; }
+
+        public List<int> Callbacks { get; private set; }
+
+        public object Result { get; private set; }
+
+        public bool IsException { get; private set;}
+    }
+}
