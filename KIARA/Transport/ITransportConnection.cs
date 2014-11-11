578c734033b7bb7f40a708db2d761733a746182b
unknown <tosp01@opeth.dfki.net>
2014-11-11 17:06:17 +0100

    Added TransportConnection Interface that should be used to establish the actual connection for the specific transport

---
+++ KIARA/Transport/ITransportConnection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA
+{
+    public interface ITransportConnection
+    {
+        /// <summary>
+        /// Raised when the connection is opened.
+        /// </summary>
+        event EventHandler Opened;
+
+        /// <summary>
+        /// Raised when the connection is closed.
+        /// </summary>
+        event EventHandler Closed;
+
+        /// <summary>
+        /// Raised when an error is occured in the connection.
+        /// </summary>
+        event EventHandler<TransportErrorEventArgs> Error;
+
+        /// <summary>
+        /// Raised when a message is received by the connection.
+        /// </summary>
+        event EventHandler<TransportMessageEventArgs> Message;
+
+        /// <summary>
+        /// Returns true if the socket is connected.
+        /// </summary>
+        bool IsConnected { get; }
+
+        /// <summary>
+        /// Opens the connection.
+        /// </summary>
+        void Open();
+
+        /// <summary>
+        /// Closes the connection.
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// Sends a message over this socket.
+        /// </summary>
+        /// <param name="message">Message to be sent.</param>
+        void Send(string message);
+    }
+}
