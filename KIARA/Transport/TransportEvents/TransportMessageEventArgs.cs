4deffb7058ed536011f3f1d7645cfe54f4b00e40
unknown <tosp01@opeth.dfki.net>
2014-11-11 17:07:14 +0100

    Added EventArg Classes

---
+++ KIARA/Transport/TransportEvents/TransportMessageEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA
+{
+    public class TransportMessageEventArgs : EventArgs
+    {
+        public IMessage Message { get;  }
+    }
+}
