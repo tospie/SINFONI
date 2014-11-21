9f4e57602530d9bca62186020502c868b8823703
unknown <tosp01@opeth.dfki.net>
2014-11-21 10:03:13 +0100

    Added Websocket Session and Adapter classes

---
+++ Transports/WebSocket/WSSessionSocketAdapter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KIARA;
+
+namespace KIARA.Transport.WebSocketTransport
+{
+    class WSSessionSocketAdapter : ITransportConnection
+    {
+        public WSSessionSocketAdapter(WSSession aSession)
+        {
+            session = aSession;
+            session.Closed += HandleClosed;
+            session.MessageReceived += HandleMessageReceived;
+        }
+
+        // This is actually another layer of message here, consider !! The message we receive here is some
+        // result of the serialized message object that we use internally. The "Message" event should thus
+        // redirect the received message to the de-serializer which then de-serializes the received string
+        // to the KIARA.Message - object
+        void HandleMessageReceived(object sender, WebSocket4Net.MessageReceivedEventArgs e)
+        {
+            if (Message != null)
+                Message(sender, new TransportMessageEventArgs(e.Message));
+        }
+
+        void HandleClosed(object sender, EventArgs e)
+        {
+            if (Closed != null)
+                Closed(sender, e);
+        }
+
+        public event EventHandler Closed;
+
+        public event EventHandler<TransportMessageEventArgs> Message;
+
+        public bool IsConnected
+        {
+            get { return session.Connected; }
+        }
+
+        public void Close()
+        {
+            session.Close();
+        }
+
+        public void Send(object message)
+        {
+            session.Send((string)message);
+        }
+
+        WSSession session;
+
+        #region This part of the interface is only used for client sockets
+        public event EventHandler Opened
+        {
+            add { throw new NotImplementedException(); }
+            remove { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler<TransportErrorEventArgs> Error
+        {
+            add { throw new NotImplementedException(); }
+            remove { throw new NotImplementedException(); }
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ Transports/WebSocket/WebSocketSocketAdapter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSocket4Net;
+using SuperSocket.ClientEngine;
+using KIARA;
+
+namespace KIARA.Transport.WebSocketTransport
+{
+    public class WebSocketSocketAdapter :  ITransportConnection
+    {
+        public WebSocketSocketAdapter(string uri)
+        {
+            this.WebSocket = new WebSocket(uri);
+            WebSocket.MessageReceived += HandleMessageReceived;
+            WebSocket.Error += HandleError;
+        }
+
+        private void HandleError(object sender, ErrorEventArgs e)
+        {
+            if (Error != null)
+                Error(sender, new TransportErrorEventArgs(e.Exception));
+        }
+
+        void HandleMessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            if (Message != null)
+                Message(sender, new TransportMessageEventArgs(e.Message));
+        }
+
+        public event EventHandler<TransportMessageEventArgs> Message;
+        public new event EventHandler<TransportErrorEventArgs> Error;
+
+        public bool IsConnected
+        {
+            get { return WebSocket.State == WebSocketState.Open; }
+        }
+
+        private WebSocket WebSocket;
+
+        public event EventHandler Opened;
+
+        public event EventHandler Closed;
+
+        event EventHandler<TransportErrorEventArgs> ITransportConnection.Error
+        {
+            add { throw new NotImplementedException(); }
+            remove { throw new NotImplementedException(); }
+        }
+
+        event EventHandler<TransportMessageEventArgs> ITransportConnection.Message
+        {
+            add { throw new NotImplementedException(); }
+            remove { throw new NotImplementedException(); }
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(object message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
