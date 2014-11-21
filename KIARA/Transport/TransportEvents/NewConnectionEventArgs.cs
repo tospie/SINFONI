dd00d98b027b3d9b6e7a423193f577ed39e4857c
unknown <tosp01@opeth.dfki.net>
2014-11-21 09:57:40 +0100

    Added EventArgumentClass for newly created connection events

---
+++ KIARA/Transport/TransportEvents/NewConnectionEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KIARA
+{
+    public class NewConnectionEventArgs : EventArgs
+    {
+        public NewConnectionEventArgs(Connection connection)
+        {
+            Connection = connection;
+        }
+
+        public Connection Connection { get; private set; }
+    }
+}
