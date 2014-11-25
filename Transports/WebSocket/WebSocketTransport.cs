b5577a2bf9786fbdaa31f74337724e7610a38686
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2014-11-25 14:57:14 +0100

    Added WebSocketTransport class that implements the ITransport Interface and can be used by Context

---
+++ Transports/WebSocket/WebSocketTransport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIARA.Transport.WebSocketTransport
+{
+    public class WebSocketTransport : ITransport
+    {
+        public string Name
+        {
+            get { return "websocket"; }
+        }
+
+        public ITransportConnectionFactory TransportConnectionFactory
+        {
+            get { return WSConnectionFactory; }
+        }
+
+        public ITransportAddress CreateAddress(string uri)
+        {
+            throw new NotImplementedException();
+        }
+
+        WebSocketConnectionFactory WSConnectionFactory = new WebSocketConnectionFactory();
+    }
+}
