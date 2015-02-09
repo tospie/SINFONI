5b5ceddd0639e139cc862d0be1fabf5af4457d15
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-02-09 14:48:18 +0100

    Added project for protocol project that implements previous FiVES custom protocol

---
+++ Protocols/FiVESJson/FiVESJsonProtocol.cs
@@ -0,0 +1,32 @@
+﻿using KIARA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiVESJson
+{
+    public class FiVESJsonProtocol : IProtocol
+    {
+        public string MimeType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Name
+        {
+            get { return "fives-json"; }
+        }
+
+        public object SerializeMessage(IMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessage DeserializeMessage(object message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
