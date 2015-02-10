c098f83d9ee81912a4b193ade41c3216d572031a
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-02-10 10:16:16 +0100

    Added exception

---
+++ Protocols/FiVESJson/InvalidMessageTypeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiVESJson
+{
+    public class InvalidMessageTypeException : Exception
+    {
+        public InvalidMessageTypeException(string attemptedType) :
+            base("Unknown message type for received message : " + attemptedType)
+        {            
+        }
+    }
+}
