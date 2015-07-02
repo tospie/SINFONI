8ca77d8fac5e4b8b15eb6a3198413c8dbde37623
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2015-07-02 13:38:12 +0200

    Added license headers to examples

--- Examples/SimpleClient/ServerSyncTools.cs
+++ Examples/SimpleClient/ServerSyncTools.cs
@@ -1,17 +1,17 @@
-﻿// This file is part of FiVES.
+﻿// This file is part of SINFONI.
 //
-// FiVES is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
 //
-// FiVES is distributed in the hope that it will be useful,
+// This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with FiVES.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 using SINFONI;
 using System;
 using System.Collections.Generic;
