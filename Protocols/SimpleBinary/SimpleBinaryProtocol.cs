e1c1343651cb1a94fcfebf88b6d7b6e6e6e1055b
Torsten Spieldenner <torsten.spieldenner@dfki.de>
2016-11-03 16:05:33 +0100

    Implemented simple Binary (De)serialization

--- Protocols/SimpleBinary/SimpleBinaryProtocol.cs
+++ Protocols/SimpleBinary/SimpleBinaryProtocol.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using SINFONI;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleBinary
 {
-    public class SimpleBinaryProtocol
+    public class SimpleBinaryProtocol : IProtocol
     {
+        public string MimeType
+        {
+            get { return "binary"; }
+        }
+
+        public string Name
+        {
+            get { return "simple-binary"; }
+        }
+
+        public object SerializeMessage(IMessage message)
+        {
+            byte[] messageAsBytestream;
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, message);
+                messageAsBytestream = ms.ToArray();
+            }
+            return messageAsBytestream;
+        }
+
+        public IMessage DeserializeMessage(object message)
+        {
+            byte[] messageBytes = (byte[])message;
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter bf = new BinaryFormatter();
+            ms.Write(messageBytes, 0, messageBytes.Length);
+            ms.Seek(0, SeekOrigin.Begin);
+            IMessage deserializedMessage = (IMessage)bf.Deserialize(ms);
+
+            return deserializedMessage;
+        }
     }
 }
